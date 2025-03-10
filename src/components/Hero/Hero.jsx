5914c9875aa390ae47e64218f5404007ec6fb557
NurAli <nuraliali2109@gmail.com>
2025-03-10 09:26:44 +0700

    update deskripsi

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,12 +13,10 @@ const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Nurali</h1>
         <p className={styles.description}>
-          I am Nurali, a graduate in Informatics Management with experience in
-          IT Support, To transition into Web Development, I have built skills in
-          Frontend and Backend Development through mini projects. I developed a
-          frontend using PHP, ReactJS, HTML, CSS, and JavaScript, while on the
-          backend, I implemented JWT authentication and CRUD features with
-          Node.js and Express.
+          I am a Full Stack Developer with an educational background in
+          Information Management. I have experience in building scalable website
+          applications, developing RESTful APIs, and optimizing database and
+          query performance using PostgreSQL and MySQL.
         </p>
         <div className={styles.buttonContainer}>
           <a
